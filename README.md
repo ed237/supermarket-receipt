7735c3d6ddc24382d446f7fd2d95961c3b5d4c60
ed237 <ekamgaing@gmail.com>
2019-01-20 19:45:56 +0100

    changements dans le code de test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # supermarket-receipt
 
-https
+
 [![Build Status](https://travis-ci.com/ed237/supermarket-receipt.svg?branch=master)](https://travis-ci.com/ed237/supermarket-receipt)
--- src/test/java/fr/esiea/AppTest.java
+++ src/test/java/fr/esiea/AppTest.java
@@ -1,38 +1,20 @@
 package fr.esiea;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+/*import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import java.util.*;*/
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    @Test
+   void test1(){
+       assertThat("toto").isEqualTo("toto");
+        }
 }
